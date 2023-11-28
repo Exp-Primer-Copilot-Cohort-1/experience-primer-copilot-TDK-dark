57b69c6ae6aafe7ba6be8c58e072b34b3914695e
TDK-dark <102588015+TDK-dark@users.noreply.github.com>
2023-11-28 01:39:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,21 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python', 'Django'];
+
+    // console.log(skills[0]);
+    // console.log(skills[1]);
+    // console.log(skills[2]);
+    // console.log(skills[3]);
+    // console.log(skills[4]);
+    // console.log(skills[5]);
+    // console.log(skills[6]);
+
+    // Loop
+    // for (var i = 0; i < skills.length; i++) {
+    //     console.log(skills[i]);
+    // }
+
+    // ForEach
+    skills.forEach(function (skill) {
+        console.log(skill);
+    });
+}
